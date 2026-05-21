fe36cd4b5b0fdbaa0ea428140c2f42be282f61f4
lemavos-3 <henriquex10q@gmail.com>
2026-05-21 20:12:14 +0000

    v9.23

--- backend/src/main/java/tech/lemnova/continuum/infra/stripe/StripeConfig.java
+++ backend/src/main/java/tech/lemnova/continuum/infra/stripe/StripeConfig.java
@@ -3,9 +3,11 @@ package tech.lemnova.continuum.infra.stripe;
 import com.stripe.Stripe;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnProperty(prefix = "stripe", name = "api.key")
 public class StripeConfig {
 
     @Value("${stripe.api.key}")
