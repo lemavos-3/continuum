7f91b823699105f3a06365355f208555b86a8b96
lemavos-3 <henriquex10q@gmail.com>
2026-07-28 18:27:50 +0000

    v3

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -413,8 +413,10 @@ export default function Profile() {
           </section>
         </div>
       </div>
-      <div className="mx-auto max-w-5xl w-full px-6 pb-6 flex justify-end text-[10px] text-white/30">
-        <span>{version}</span>
+      <div className="mx-auto max-w-5xl px-6 pb-6">
+        <div className="w-full text-right text-[10px] text-white/30">
+          {version}
+        </div>
       </div>
       <MarkdownImportDialog
         open={importOpen}
